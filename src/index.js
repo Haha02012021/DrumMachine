e2c5bbcac9d5a3098dae8d0b9721998adf915eec
20020399 Đào Thu Hằng <78419055+Haha02012021@users.noreply.github.com>
2021-10-26 16:23:03 +0700

    remove un-needed files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { combineReducers, createStore } from 'redux';
 import { connect } from 'react-redux';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+
 
 const bankOne = [
   {
@@ -424,8 +422,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
